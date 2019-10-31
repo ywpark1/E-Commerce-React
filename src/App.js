35950557480c4c4bdbce5246996299e3507c35c3
Yeon Woo Park <ywpark1@localhost.localdomain>
2019-10-30 20:33:25 -0400

    Divide homepage into smaller components. Add image with hover

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import HomePage from "./homepage.component";
-import "./homepage.styles.scss";
+import HomePage from "./pages/homepage/homepage.component";
+import "./pages/homepage/homepage.styles.scss";
 function App() {
   return (
     <div>
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Directory from "../../components/directory/directory.component";
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <div className="Homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
